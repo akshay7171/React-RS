be5338f77b749a6842ad859b6e27cc46ad231355
Akshay Bhagat <84901485+akshay7171@users.noreply.github.com>
2021-12-29 11:13:25 +0530

    Add files via upload

---
+++ JavaScript/pract.js
@@ -0,0 +1,52 @@
+console.log("hello javaScript");
+//document.getElementById("div1").textContent = "Hello world!";
+
+// let a = 30;
+// const b = 50;
+
+// console.log(a * b);
+
+// const name = 'akshay'
+// const f_name = "akshay"
+// const a = 'akshay'
+// const b = "akshay"
+// const str = "hello javascript"
+// console.log(str.length)
+// console.log(str[str.length - 1])
+
+const arr = [10, 20, 40]
+    // console.log(arr[2])
+    // arr.push(25) //insert right side
+    // console.log(arr)
+    // arr.push(70)
+    // arr.pop() //remove right side
+    // console.log(arr)
+    // arr.shift() //remove left side
+    // console.log(arr)
+    // console.log(arr)
+    // arr.unshift(15) //add right side
+    // console.log(arr) //
+
+
+arr.splice(2, 2, 'May');
+console.log(arr);
+
+// function test(a, b) {
+//     // console.log("hello")
+//     return a + b;
+// }
+// console.log(test(33, 12)) //function call
+
+// function add(a, b) {
+//     console.log(a + b) //function default
+// }
+// add(50, 20) //function call
+
+// const a = 10;
+
+// function test() {
+//     const b = 5;
+//     console.log(a)
+// }
+// test()
+// console.log(b); //error outside function
